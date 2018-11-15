df4d1e46ad631ecc4d8161f408c042080c6c6530
Frank <fengxuting@gmail.com>
2018-11-15 11:39:10 +0800

    Fix

--- server/config.local.js
+++ server/config.local.js
@@ -5,7 +5,7 @@ let redis = {
   host: process.env.REDIS_IP || '127.0.0.1',
   password: process.env.REDIS_PWD,
 };
-let whiteList = process.env.WHITE_LIST || '127.0.0.1';
+let whiteList = process.env.WHITE_LIST || '127.0.0.1,221.217.90.174';
 module.exports = {
   appName: 'rand',
   host: process.env.HOST || '0.0.0.0',
